8be00f667cb97fa0f85c8ebe472ee76f7cfdd039
David Mader <david.mader@hak-kitz.ac.at>
2021-11-25 09:03:34 +0100

    Tower full changed

--- Towerdefense/game.cs
+++ Towerdefense/game.cs
@@ -194,7 +194,7 @@ namespace Towerdefense
             {
                 if (currentnumber == 1)
                 {
-                    pb_tower1.BackColor = Color.Green;
+                    pb_tower1.BackColor = Color.Green;          
                 }
                 else if (currentnumber == 2)
                 {
@@ -211,6 +211,26 @@ namespace Towerdefense
                 else if (currentnumber == 5)
                 {
                     pb_tower5.BackColor = Color.Green;
+                }
+                else if (currentnumber == 6)
+                {
+                    pb_tower6.BackColor = Color.Green;
+                }
+                else if (currentnumber == 7)
+                {
+                    pb_tower7.BackColor = Color.Green;
+                }
+                else if (currentnumber == 8)
+                {
+                    pb_tower8.BackColor = Color.Green;
+                }
+                else if (currentnumber == 9)
+                {
+                    pb_tower9.BackColor = Color.Green;
+                }
+                else if (currentnumber == 10)
+                {
+                    pb_tower10.BackColor = Color.Green;
                 }
             }
             if (towerselect.changeTowerNumber == 2)
@@ -235,39 +255,158 @@ namespace Towerdefense
                 {
                     pb_tower5.BackColor = Color.Blue;
                 }
+                else if (currentnumber == 6)
+                {
+                    pb_tower6.BackColor = Color.Blue;
+                }
+                else if (currentnumber == 7)
+                {
+                    pb_tower7.BackColor = Color.Blue;
+                }
+                else if (currentnumber == 8)
+                {
+                    pb_tower8.BackColor = Color.Blue;
+                }
+                else if (currentnumber == 9)
+                {
+                    pb_tower9.BackColor = Color.Blue;
+                }
+                else if (currentnumber == 10)
+                {
+                    pb_tower10.BackColor = Color.Blue;
+                }
             }
             if (towerselect.changeTowerNumber == 3)
             {
+                if (currentnumber == 1)
+                {
+                    pb_tower1.BackColor = Color.Red;
+                }
+                else if (currentnumber == 2)
+                {
+                    pb_tower2.BackColor = Color.Red;
+                }
+                else if (currentnumber == 3)
+                {
+                    pb_tower3.BackColor = Color.Red;
+                }
+                else if (currentnumber == 4)
+                {
+                    pb_tower4.BackColor = Color.Red;
+                }
+                else if (currentnumber == 5)
+                {
+                    pb_tower5.BackColor = Color.Red;
+                }
+                else if (currentnumber == 6)
+                {
+                    pb_tower6.BackColor = Color.Red;
+                }
+                else if (currentnumber == 7)
+                {
+                    pb_tower7.BackColor = Color.Red;
+                }
+                else if (currentnumber == 8)
+                {
+                    pb_tower8.BackColor = Color.Red;
+                }
+                else if (currentnumber == 9)
+                {
+                    pb_tower9.BackColor = Color.Red;
+                }
+                else if (currentnumber == 10)
+                {
+                    pb_tower10.BackColor = Color.Red;
+                }
 
             }
             if (towerselect.changeTowerNumber == 4)
             {
-
+                if (currentnumber == 1)
+                {
+                    pb_tower1.BackColor = Color.Yellow;
+                }
+                else if (currentnumber == 2)
+                {
+                    pb_tower2.BackColor = Color.Yellow;
+                }
+                else if (currentnumber == 3)
+                {
+                    pb_tower3.BackColor = Color.Yellow;
+                }
+                else if (currentnumber == 4)
+                {
+                    pb_tower4.BackColor = Color.Yellow;
+                }
+                else if (currentnumber == 5)
+                {
+                    pb_tower5.BackColor = Color.Yellow;
+                }
+                else if (currentnumber == 6)
+                {
+                    pb_tower6.BackColor = Color.Yellow;
+                }
+                else if (currentnumber == 7)
+                {
+                    pb_tower7.BackColor = Color.Yellow;
+                }
+                else if (currentnumber == 8)
+                {
+                    pb_tower8.BackColor = Color.Yellow;
+                }
+                else if (currentnumber == 9)
+                {
+                    pb_tower9.BackColor = Color.Yellow;
+                }
+                else if (currentnumber == 10)
+                {
+                    pb_tower10.BackColor = Color.Yellow;
+                }
             }
             if (towerselect.changeTowerNumber == 5)
             {
-
+                if (currentnumber == 1)
+                {
+                    pb_tower1.BackColor = Color.Orange;
+                }
+                else if (currentnumber == 2)
+                {
+                    pb_tower2.BackColor = Color.Orange;
+                }
+                else if (currentnumber == 3)
+                {
+                    pb_tower3.BackColor = Color.Orange;
+                }
+                else if (currentnumber == 4)
+                {
+                    pb_tower4.BackColor = Color.Orange;
+                }
+                else if (currentnumber == 5)
+                {
+                    pb_tower5.BackColor = Color.Orange;
+                }
+                else if (currentnumber == 6)
+                {
+                    pb_tower6.BackColor = Color.Orange;
+                }
+                else if (currentnumber == 7)
+                {
+                    pb_tower7.BackColor = Color.Orange;
+                }
+                else if (currentnumber == 8)
+                {
+                    pb_tower8.BackColor = Color.Orange;
+                }
+                else if (currentnumber == 9)
+                {
+                    pb_tower9.BackColor = Color.Orange;
+                }
+                else if (currentnumber == 10)
+                {
+                    pb_tower10.BackColor = Color.Orange;
+                }
             }
-            if (towerselect.changeTowerNumber == 6)
-            {
-
-            }
-            if (towerselect.changeTowerNumber == 7)
-            {
-
-            }
-            if (towerselect.changeTowerNumber == 8)
-            {
-
-            }
-            if (towerselect.changeTowerNumber == 9)
-            {
-
-            }
-            if (towerselect.changeTowerNumber == 10)
-            {
-
-            }
+            
             #endregion
 
         }
