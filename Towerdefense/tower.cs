aeda3d050ea91d33db97f29c1938a932ab8d9a53
David Mader <david.mader@hak-kitz.ac.at>
2022-01-18 20:20:22 +0100

    all tower shoot

--- Towerdefense/tower.cs
+++ Towerdefense/tower.cs
@@ -12,30 +12,44 @@ using System.Resources;
 namespace Towerdefense
 {
     class tower
-    { 
+    {
+        #region Lists and Panels for Towers
         public static List<Panel> panelmagetowershot = new List<Panel>();
         public static List<Panel> panelarchertowershot = new List<Panel>();
+        public static List<Panel> panelBombTowerShot = new List<Panel>();
+        public static List<Panel> panelNinjaTowerShot = new List<Panel>();
+        public static List<Panel> panelMachineGunTowerShot = new List<Panel>();
 
         public static Panel tempmage = new Panel();
         public static Panel temparcher = new Panel();
+        public static Panel tempBomb = new Panel();
+        public static Panel tempNinja = new Panel();
+        public static Panel tempMachinegun = new Panel();
+        #endregion
 
         public static int bulletSpeedX = enemy.pb.Location.X;
         public static int bulletSpeedY = enemy.pb.Location.Y;
 
-        public static bool activemageTower = false;
-        public static bool activeArcherTower = false;
+        public static int mageTowerPrice = 200;
+        public static int archerTowerPrice = 250;
+        public static int bombTowerPrice = 300;
+        public static int ninjaTowerPrice = 350;
+        public static int machinegunTowerPrice = 400;
 
 
+
+        #region Towermethodes
         public static void magetowerShoot(int locationX, int locationY, int bulletspeed)
         {
             if (!panelmagetowershot.Contains(tempmage))
             {
                 tempmage = new Panel();
+                tempmage.Name = "mage";
                 tempmage.Location = new Point(locationX, locationY);
                 tempmage.BackColor = Color.Red;
                 tempmage.Height = 3;
                 tempmage.Width = 30;
-                tempmage.Tag = "bulletmage";
+                tempmage.Tag = "bullet";
                 panelmagetowershot.Add(tempmage);
                 game1.ActiveForm.Controls.Add(tempmage);
             }
@@ -48,16 +62,17 @@ namespace Towerdefense
             tempmage.Left -= bulletspeed;
         }
 
-        public static void archertowerShoot(int locationX, int locationY,int bulletspeed) 
+        public static void archertowerShoot(int locationX, int locationY, int bulletspeed)
         {
             if (!panelarchertowershot.Contains(temparcher))
             {
-                temparcher = new Panel();   
+                temparcher = new Panel();
+                temparcher.Name = "archer";
                 temparcher.Location = new Point(locationX, locationY);
                 temparcher.BackColor = Color.Blue;
                 temparcher.Height = 3;
                 temparcher.Width = 30;
-                temparcher.Tag = "bulletmage";
+                temparcher.Tag = "bullet";
                 panelarchertowershot.Add(temparcher);
                 game1.ActiveForm.Controls.Add(temparcher);
             }
@@ -72,7 +87,77 @@ namespace Towerdefense
 
         }
 
-        public static void ButtonDeactivateTowers(PictureBox pbTower, Button towerButton ) 
+        public static void bombTowerShoot(int locationX, int locationY, int bulletspeed)
+        {
+            if (!panelBombTowerShot.Contains(tempBomb))
+            {
+                tempBomb = new Panel();
+                tempBomb.Name = "bomb";
+                tempBomb.Location = new Point(locationX, locationY);
+                tempBomb.BackColor = Color.Orange;
+                tempBomb.Height = 3;
+                tempBomb.Width = 30;
+                tempBomb.Tag = "bullet";
+                panelBombTowerShot.Add(tempBomb);
+                game1.ActiveForm.Controls.Add(tempBomb);
+            }
+            if (tempBomb.Left < locationX - 100)
+            {
+                panelBombTowerShot.Remove(tempBomb);
+                tempBomb.Location = new Point(locationX, locationY);
+
+            }
+            tempBomb.Left -= bulletspeed;
+        }
+
+        public static void ninjaTowerShoot(int locationX, int locationY, int bulletspeed)
+        {
+            if (!panelNinjaTowerShot.Contains(tempNinja))
+            {
+                tempNinja = new Panel();
+                tempNinja.Name = "ninja";
+                tempNinja.Location = new Point(locationX, locationY);
+                tempNinja.BackColor = Color.Green;
+                tempNinja.Height = 3;
+                tempNinja.Width = 30;
+                tempNinja.Tag = "bullet";
+                panelNinjaTowerShot.Add(tempNinja);
+                game1.ActiveForm.Controls.Add(tempNinja);
+            }
+            if (tempNinja.Left < locationX - 100)
+            {
+                panelBombTowerShot.Remove(tempmage);
+                tempNinja.Location = new Point(locationX, locationY);
+
+            }
+            tempNinja.Left -= bulletspeed;
+        }
+
+        public static void machinegunTowerShoot(int locationX, int locationY, int bulletspeed)
+        {
+            if (!panelMachineGunTowerShot.Contains(tempMachinegun))
+            {
+                tempMachinegun = new Panel();
+                tempMachinegun.Name = "machinegun";
+                tempMachinegun.Location = new Point(locationX, locationY);
+                tempMachinegun.BackColor = Color.Yellow;
+                tempMachinegun.Height = 3;
+                tempMachinegun.Width = 30;
+                tempMachinegun.Tag = "bullet";
+                panelMachineGunTowerShot.Add(tempMachinegun);
+                game1.ActiveForm.Controls.Add(tempMachinegun);
+            }
+            if (tempMachinegun.Left < locationX - 100)
+            {
+                panelMachineGunTowerShot.Remove(tempMachinegun);
+                tempMachinegun.Location = new Point(locationX, locationY);
+
+            }
+            tempMachinegun.Left -= bulletspeed;
+        }
+        #endregion 
+
+        public static void UnvisibleButton()
         {
             foreach (Control x in game1.ActiveForm.Controls)
             {
@@ -81,8 +166,12 @@ namespace Towerdefense
                     x.Visible = false;
                 }
             }
+        }
+        public static void ButtonDeactivateTowers(PictureBox pbTower, Button towerButton ) 
+        {
+            UnvisibleButton();
 
-                if(game1.TowerType == "mage")
+                if (game1.TowerType == "mage")
                 {
                     pbTower.BackgroundImage = Properties.Resources.mageTower;
                     pbTower.Tag = "mageTower";
@@ -129,15 +218,74 @@ namespace Towerdefense
 
         public static void checkTowerPlacement() 
         {
-            foreach(Control x in game1.ActiveForm.Controls)
+            foreach (Control x in game1.ActiveForm.Controls)
             {
                 if(x is PictureBox && x.Tag == "mageTower")
                 {
-                    magetowerShoot(x.Location.X, x.Location.Y, 5);
+                    //if(game1.Coins >= mageTowerPrice)
+                    //{
+                        magetowerShoot(x.Location.X, x.Location.Y, 5);
+                        //foreach(Button b in game1.ActiveForm.Controls)
+                        //{
+                        // if ()
+                        //    {
+                                game1.Coins = game1.Coins - mageTowerPrice;
+                        //    }
+                        //}     
+                    //}
+                    //else
+                    //{
+                    //    MessageBox.Show("Sie haben zu wenig Coins um diesen Tower zu plazieren", "Zu Wenig Coins", MessageBoxButtons.OK);
+                    //}
                 }
                 else if(x is PictureBox && x.Tag == "archerTower")
                 {
-                    archertowerShoot(x.Location.X, x.Location.Y, 5);
+                    //if(game1.Coins >= archerTowerPrice)
+                    //{
+                        archertowerShoot(x.Location.X, x.Location.Y, 5);
+                        game1.Coins = game1.Coins - archerTowerPrice;
+                    //}
+                   // else
+                    //{
+                      //MessageBox.Show("Sie haben zu wenig Coins um diesen Tower zu plazieren", "Zu Wenig Coins", MessageBoxButtons.OK);
+                    //}
+                }
+                else if (x is PictureBox && x.Tag == "bombTower")
+                {
+                    //if(game1.Coins >= bombTowerPrice)
+                    //{
+                        bombTowerShoot(x.Location.X, x.Location.Y, 5);
+                        game1.Coins = game1.Coins - bombTowerPrice;
+                    //}
+                    //else
+                    //{
+                        MessageBox.Show("Sie haben zu wenig Coins um diesen Tower zu plazieren", "Zu Wenig Coins", MessageBoxButtons.OK);
+                    //}
+                }
+                else if (x is PictureBox && x.Tag == "ninjaTower")
+                {
+                    //if(game1.Coins >= ninjaTowerPrice)
+                    //{
+                        ninjaTowerShoot(x.Location.X, x.Location.Y, 5);
+                        game1.Coins = game1.Coins - ninjaTowerPrice;
+                    //}
+                    //else
+                    //{
+                        MessageBox.Show("Sie haben zu wenig Coins um diesen Tower zu plazieren", "Zu Wenig Coins", MessageBoxButtons.OK);
+                    //}
+
+                }
+                else if (x is PictureBox && x.Tag == "machinegun")
+                {
+                    //if(game1.Coins >= machinegunTowerPrice)
+                    //{
+                        machinegunTowerShoot(x.Location.X, x.Location.Y, 20);
+                        game1.Coins = game1.Coins - machinegunTowerPrice;
+                   // }
+                    //else
+                    //{
+                        MessageBox.Show("Sie haben zu wenig Coins um diesen Tower zu plazieren", "Zu Wenig Coins", MessageBoxButtons.OK);
+                    //}
                 }
             }
         }
